4b758efda37f99c1cdc32721cdc834ce211be656
lcsatchell <119857682+lcsatchell@users.noreply.github.com>
2025-04-14 13:24:46 +0100

    Force main.mm compile as objc++

--- templates/ios/template/{{app.file}}/Classes/Main.mm
+++ templates/ios/template/{{app.file}}/Classes/Main.mm
@@ -1,5 +1,67 @@
 #include <stdio.h>
 
+// --------------------------------------
+// 1) Overriding SDL's appDelegate
+// to provide access to didFinishLaunchingWithOptions
+// --------------------------------------
+#import <UIKit/UIKit.h>
+
+::if SET_FIREBASE_INTEGRATION::
+// Import Firebase (if you want to use Firebase):
+#import <FirebaseCore/FirebaseCore.h>
+::end::
+
+// refers to the copy of SDL_uikitappdelegate.h also in goose templates:
+#import "SDL_uikitappdelegate.h"
+
+// --- Subclass SDLUIKitDelegate ---
+@interface GooseDelegate : SDLUIKitDelegate
+@end
+
+@implementation GooseDelegate
+
+	// -------------------------------------
+	// didFinishLaunchingWithOptions 
+	// -------------------------------------
+	- (BOOL)application:(UIApplication *)application
+		didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+	{
+		// Call SDL’s normal setup first
+		BOOL ret = [super application:application didFinishLaunchingWithOptions:launchOptions];
+		
+		// Your custom code goes here:
+		NSLog(@"GooseDelegate is now in control!");
+
+	::if SET_FIREBASE_INTEGRATION::
+		// init Firebase:
+		[FIRApp configure];
+	::end::
+
+		return ret;
+	}
+
+@end
+
+// --- Category to tell SDL to use GooseDelegate ---
+@interface SDLUIKitDelegate (extra)
+@end
+
+@implementation SDLUIKitDelegate (extra)
+	// -------------------------------------
+	// getAppDelegateClassName
+	// Tells SDL to use our overriden delegate instead of the base SDLUIKitDelegate.
+	// -------------------------------------
+	+ (NSString *)getAppDelegateClassName 
+	{
+		return @"GooseDelegate";
+	}
+@end
+
+
+// --------------------------------------
+// 2) Existing Haxe/Lime bridging code
+//    (Unchanged from lime's original Main.mm template)
+// --------------------------------------
 extern "C" const char *hxRunLibrary ();
 extern "C" void hxcpp_set_top_of_stack ();
 
