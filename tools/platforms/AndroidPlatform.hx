710fa46b1d455f532712856a272aa135e4f618ee
Greg Denness <72026480+gregdenness@users.noreply.github.com>
2025-11-25 14:46:23 +0000

    changed comment for amazon build

--- tools/platforms/AndroidPlatform.hx
+++ tools/platforms/AndroidPlatform.hx
@@ -120,11 +120,11 @@ class AndroidPlatform extends PlatformTarget
 		trace("==============================================================" );
 		trace("Building for Android, options are:" );
 		trace("" );
-		trace("lime test android -debug -quick	: arm64 only, fast test builds" );
-		trace("lime test android -debug -no_arm64	: arm7 only, testing on arm7 devices" );
-		trace("lime test android -debug -simulator	: for testing on simulator" );
-		trace("lime test android -release		: APK for Amazon submission" );
-		trace("lime test android -bundle		: AAB for Google Play submission" );
+		trace("lime test android -debug -quick			: arm64 only, fast test builds" );
+		trace("lime test android -debug -no_arm64		: arm7 only, testing on arm7 devices" );
+		trace("lime test android -debug -simulator		: for testing on simulator" );
+		trace("lime test android -release -amazon		: APK for Amazon submission" );
+		trace("lime test android -bundle				: AAB for Google Play submission" );
 		trace("" );
 		trace("==============================================================" );
 
